5cdc89d4725cb23c42296ccb7879aa8ea40007b6
mohsin-356 <mohsinsaeed356@gmail.com>
2025-01-26 23:59:41 +0500

    add basic CSS reset and layout styles

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the API!');
+  res.render('index');
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
